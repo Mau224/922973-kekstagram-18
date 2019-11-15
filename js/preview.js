948ece68c44a29efa4970282ede1d4036040fe93
Mau224 <46903680+Mau224@users.noreply.github.com>
2019-11-15 20:29:15 +0300

    комменты

--- js/preview.js
+++ js/preview.js
@@ -6,67 +6,51 @@
   var bigPicture = document.querySelector('.big-picture');
   var socialComments = bigPicture.querySelector('.social__comments');
   var renderCommentCout = document.querySelector('.social__comment-count');
-
-  var comments = [];
+  var indexComment = null;
 
   var bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
   var commentLoad = bigPicture.querySelector('.comments-loader');
-  // commentLoad.classList.add('visually-hidden');
-  window.photoClosed = function () {
-    window.photoElement.addEventListener('click', function () {
-      bigPicture.classList.remove('hidden');
+
+  window.photoElement.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.util.ENTER_KEY) {
       createBigPicture(window.photo);
-    });
-
-    // обработчик по enter
-    window.photoElement.addEventListener('keydown', function (evt) {
-      if (evt.keyCode === window.util.ENTER_KEY) {
-        createBigPicture(window.photo);
-      }
-    });
-    return window.photoElement;
-  };
-
-
-  // Закрывает пользовательский пост
-  var closeBigPicture = function () {
-    bigPicture.classList.add('hidden');
-
-    bigPictureClose.removeEventListener('click', onBigPictureCloseClick);
-    document.removeEventListener('keydown', onBigPictureEscPress);
-  };
-
-  var onBigPictureCloseClick = function () {
-    closeBigPicture();
-  };
-
-  var onBigPictureEscPress = function (evt) {
-    if (evt.keyCode === window.util.ESC_KEY) {
-      closeBigPicture();
     }
-  };
+  });
 
+
+  var renderComment = function (template, comment) {
+    var element = template.cloneNode(true); // клонирование тег li и дети
+    var commentImg = element.querySelector('img');
+    commentImg.src = comment.avatar;
+    commentImg.alt = comment.names;
+    var socialText = element.querySelector('.social__text');
+    socialText.textContent = comment.message;// длина массива
+    return element;
+  };
 
   var renderComments = function (data) {
+    var template = socialComments.querySelector('.social__comment');
 
-    var callTemplate = socialComments.querySelector('.social__comment');
     socialComments.innerHTML = '';
 
     var commentList = document.createDocumentFragment();
 
     for (var i = 0; i < data.length; i++) {
-      var element = callTemplate.cloneNode(true); // клонирование тег li и дети
-      var commentImg = element.querySelector('img');
-      commentImg.src = data[i].avatar;
-      commentImg.alt = data[i].names;
-      var socialText = element.querySelector('.social__text');
-      socialText.textContent = data[i].message;// длина массива
-      commentList.appendChild(element);// добавляет клонированый li
+      commentList.appendChild(renderComment(template, data[i]));// добавляет клонированый li
     }
+
     socialComments.appendChild(commentList);// должен вставить в ul
+
+    return data.length;
+  };
+
+  var printCommentCount = function (element, count, total) {
+    element.textContent = count + ' ' + 'из' + ' ' + total + ' ' + 'коментариев';
   };
 
   var createBigPicture = function (photoInfo) {
+    indexComment = 0;
+
     var bigPictureElement = bigPicture.querySelector('.big-picture__img img');
     bigPictureElement.src = photoInfo.url;
 
@@ -83,21 +67,49 @@
     var socialText = bigPicture.querySelector('.social__text');
     socialText.textContent = photoInfo.comments[0].message;
 
-    var onCommentsLoaderClick = function () {
-      var nextComment = comments.slice(socialComments, socialComments + COMMENTS_MAX);
-      renderComments(nextComment);
-      socialComments += COMMENTS_MAX;
-      if (socialComments >= comments.length || comments.length < COMMENTS_MAX) {
-        commentLoad.classList.add('.vissualy-hidden');
+    renderCommentCout.innerHTML = '';
+
+    indexComment = renderComments(photoInfo.comments.slice(0, COMMENTS_MAX));
+
+    printCommentCount(renderCommentCout, indexComment, photoInfo.comments.length);
+
+    // Закрывает пользовательский пост
+    var closeBigPicture = function () {
+      bigPicture.classList.add('hidden');
+
+      bigPictureClose.removeEventListener('click', onBigPictureCloseClick);
+      document.removeEventListener('keydown', onBigPictureEscPress);
+      commentLoad.removeEventListener('click', onCommentsLoaderClick);
+    };
+
+    var onBigPictureCloseClick = function () {
+      closeBigPicture();
+    };
+
+    var onBigPictureEscPress = function (evt) {
+      if (evt.keyCode === window.util.ESC_KEY) {
+        closeBigPicture();
       }
     };
 
-    renderCommentCout.innerHTML = '';
-    renderCommentCout.textContent = photoInfo.comments.length + ' ' + 'из' + ' ' + photoInfo.comments.length + ' ' + 'коментариев';
+    var onCommentsLoaderClick = function () {
+      var array = photoInfo.comments.slice(0, indexComment + COMMENTS_MAX);
+      indexComment = renderComments(array);
 
-    renderComments(photoInfo.comments);
+      if (photoInfo.comments.length <= array.length) {
+        commentLoad.classList.add('hidden');
+      }
+
+      printCommentCount(renderCommentCout, array.length, photoInfo.comments.length);
+    };
 
     commentLoad.addEventListener('click', onCommentsLoaderClick);
+
+    if (photoInfo.comments.length > COMMENTS_MAX) {
+      commentLoad.classList.remove('hidden');
+    } else {
+      commentLoad.classList.add('hidden');
+    }
 
     bigPictureClose.addEventListener('click', onBigPictureCloseClick);
 
